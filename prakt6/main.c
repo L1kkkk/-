87643cba97253a6b5601812a0c271d7c0307d930
L1kkkk <kn23-m.rudenko@nubip.edu.ua>
2024-07-09 23:56:53 +0800

    Практична 6

---
+++ prakt6/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int x1, y1, x2, y2;
+    printf("Введіть 4  координати : ");
+    scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+
+    double length = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+
+    printf("Довжина: %.6lf\n", length);
+
+    return 0;
+}
